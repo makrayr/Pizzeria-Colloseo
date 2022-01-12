8d302db07564698fba9d6caaed59a250e73ddee0
Makray Robert <www.makrayr@gmail.com>
2022-01-12 21:38:40 +0200

    Lab. 12

---
+++ Makray_Robert_Lab.12/Makray_Robert_Lab12/Makray_Robert_Lab12/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Makray_Robert_Lab12.Data;
+
+namespace Makray_Robert_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Makray_Robert_Lab.12/Makray_Robert_Lab12/Makray_Robert_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using Makray_Robert_Lab12.Models;
+
+namespace Makray_Robert_Lab12.Data
+{
+    internal interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
